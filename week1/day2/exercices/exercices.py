3b2f4ddaaace264ce209ce22990151fc3d6cfb5f
AmazzarK <amazzar.consult@gmail.com>
2025-08-12 16:48:00 +0100

    exo 3

--- week1/day2/exercices/exercices.py
+++ week1/day2/exercices/exercices.py
@@ -26,10 +26,10 @@ print(cinemax('beth', 13))
 print(cinemax('morty', 5)) 
 print(cinemax('summer', 8))  
 
-#Exercise 3 : zara
+#Exercise 3 : Zara
 
-
-zara_dict = {
+#dictionary brand qst1
+brand_dict = {
 
     "name": "Zara",
     "creation_date": 1975,
@@ -44,6 +44,86 @@ zara_dict = {
     }
 
 }
+print(brand_dict)
+#add new key number_stores qst2
+brand_dict['number_stores'] = '2'
+#test
+print(brand_dict['number_stores'])
+
+#explain how Zara clients are qst3  
+type_of_clothes = brand_dict['type_of_clothes'] 
+print(f"Zara clients are interested in {', '.join(type_of_clothes)} clothes.")
+
+#add  new key country_creation qst4
+brand_dict['country_creation'] = 'Spain'
+#test
+print(brand_dict['country_creation'])
 
 
+#function to check if a key exists qst5
+def key_exists(brand_dict, international_competitors):
+    if international_competitors in brand_dict:
+
+        brand_dict[international_competitors].insert(0, 'Desigual')
+        return print(f"{international_competitors} exists in the dictionary")
+    else:
+        return print(f"{international_competitors} does not exist in the dictionary")
+    
+key_exists(brand_dict, 'international_competitors')
+
+#test 
+print(brand_dict['international_competitors'])
+
+#del key qst6
+del brand_dict['creation_date']
+#test   
+print(brand_dict)
+
+#print info about last international competitor qst7
+last_competitor = brand_dict['international_competitors'][-1]
+
+print(f"The last international competitor is {last_competitor}")
+
+#print the major color in the US qst8
+
+colors = brand_dict['major_color']['US']
+print(f"The major color in the US are : { ','.join(colors)}")
+
+#Print the amount of key value pairs qst9
+print(f"The amount of key value pairs in the dictionary is: {len(brand_dict)}")
+
+#Print the keys of the dictionary qst10
+
+print("The keys of the dictionary are:" + ', '.join(brand_dict.keys()))
+#dict_more_on_zara qst 11
+
+dict_more_on_zara = {
+        "creation_date": 1975,
+        "number_stores": 10000
+}
+
+#test
+print(f"Zara was created in {dict_more_on_zara['creation_date']} and has {dict_more_on_zara['number_stores']} stores worldwide.")
+
+#adding method qst12
+print("Before adding the new information, the brand dictionary is:")
+print(brand_dict)
+
+brand_dict.update(dict_more_on_zara)
+print("After adding the new information, the brand dictionary is:")
+print(brand_dict)   
+
+#number_stores  
+
+
+
+
+
+
+
+
+
+
+
+#exercice 4
 
