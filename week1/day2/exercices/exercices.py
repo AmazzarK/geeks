3cb9aa6af98600110450f92b792bf852d42cf94c
AmazzarK <amazzar.consult@gmail.com>
2025-08-12 15:45:13 +0100

    exo3

--- week1/day2/exercices/exercices.py
+++ week1/day2/exercices/exercices.py
@@ -26,6 +26,24 @@ print(cinemax('beth', 13))
 print(cinemax('morty', 5)) 
 print(cinemax('summer', 8))  
 
+#Exercise 3 : zara
+
+
+zara_dict = {
+
+    "name": "Zara",
+    "creation_date": 1975,
+    "creator_name": "Amancio Ortega Gaona",
+    "type_of_clothes":["men", "women", "children", "home" ],
+    "international_competitors": ["Gap", "H&M", "Benetton"],
+    "number_stores": 7000,
+    "major_color": {
+        "France": "blue",
+        "Spain": "red",
+        "US": ["pink", "green"]
+    }
+
+}
 
 
 
