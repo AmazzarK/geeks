16b649432134d6d84aac5e4c20c9bb6d5850b949
AmazzarK <amazzar.consult@gmail.com>
2025-08-12 15:29:23 +0100

    exo 1 and 2

---
+++ week1/day2/exercices/exercices.py
@@ -0,0 +1,31 @@
+#Exercise 1 : Convert lists into dictionaries
+
+
+my_dict = dict(zip(['Ten', 'Twenty', 'Thirty'], [10, 20, 30]))  
+
+print(my_dict) 
+
+#Exercise 2 : cinemax
+
+#function
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+family_list = list(family.items())
+
+def cinemax(name, age):
+    if age < 3:
+        return f"{name} gets a free ticket."
+    elif 3 <= age < 12:
+        return f"{name} gets a ticket for $10."
+    else:
+        return f"{name} gets a ticket for $15."
+
+
+#test
+print(cinemax('rick', 43))  
+print(cinemax('beth', 13)) 
+print(cinemax('morty', 5)) 
+print(cinemax('summer', 8))  
+
+
+
+
