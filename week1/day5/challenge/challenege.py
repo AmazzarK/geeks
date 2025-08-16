50c2ed58fe0344da0a87be06105a6cf0013844f8
AmazzarK <amazzar.consult@gmail.com>
2025-08-16 11:34:45 +0100

    challenge

---
+++ week1/day5/challenge/challenege.py
@@ -0,0 +1,47 @@
+import math
+
+class Circle:
+    def __init__(self, radius=None, diameter=None):
+        if radius is not None:
+            self.radius = radius
+        elif diameter is not None:
+            self.radius = diameter / 2
+        else:
+            raise ValueError("You must provide either radius or diameter.")
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def __str__(self):
+        return f"Circle with radius: {self.radius:.2f}, diameter: {self.diameter:.2f}, area: {self.area():.2f}"
+
+    def __add__(self, other):
+        if isinstance(other, Circle):
+            return Circle(radius=self.radius + other.radius)
+        return NotImplemented
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+c1 = Circle(radius=3)
+c2 = Circle(diameter=10)
+
+print(c1)  
+print(c2)
+c3 = c1 + c2
+print(c3)  
+
+print(c1 < c2)  
+print(c1 == Circle(radius=3))  
+
+circles = [c1, c2, c3]
+circles.sort()
+for circle in circles:
+    print(circle)
