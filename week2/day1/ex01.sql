f404cd08ee770def476ab5d2c560a9b8485bf9e5
AmazzarK <amazzar.consult@gmail.com>
2025-08-18 18:33:33 +0100

    sql ex1

---
+++ week2/day1/ex01.sql
@@ -0,0 +1,56 @@
+CREATE TABLE items (
+    ID_Item INT PRIMARY KEY,
+    Title_Item VARCHAR(100),
+    Price INT
+);
+
+
+CREATE TABLE customers (
+    ID_Customer INT PRIMARY KEY,
+    First_name VARCHAR(100),
+    last_name VARCHAR(100),
+);
+
+
+INSERT INTO items (Title_Item, Price)
+VALUES ("Small Desk", 100);
+
+
+INSERT INTO items (Title_Item, Price)
+VALUES ("Large desk", 30);
+
+
+INSERT INTO items (Title_Item, Price)
+VALUES ("Fan ", 80);
+
+
+
+INSERT INTO customers (First_name, last_name)
+VALUES ( "Greg","Jones");
+
+INSERT INTO customers (First_name, last_name)
+VALUES ( "Sandra" , "Jones");
+
+INSERT INTO customers (First_name, last_name)
+VALUES ( "Scott" , "Scott");
+
+INSERT INTO customers (First_name, last_name)
+VALUES ( "Trevor" , "Green");
+
+INSERT INTO customers (First_name, last_name)
+VALUES ( "Melanie" , "Johnson");
+
+
+SELECT * from items
+
+SELECT * from items WHERE price > 80
+
+SELECT * from items WHERE price = 300
+
+SELECT * from customers WHERE last_name="Smith"
+
+SELECT * from customers WHERE last_name="Jones"
+
+SELECT *
+FROM customers
+WHERE firstname <> 'Scott';
